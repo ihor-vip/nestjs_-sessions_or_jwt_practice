cab5e3d37bdb89a9abd611e02bbb931a00efe566
ihor-vip <vip20140902@gmail.com>
2022-07-25 07:42:11 +0300

    nestjs express-session and jwt

---
+++ src/auth/local-auth.guard.ts
@@ -0,0 +1,31 @@
+// Using express-session cookies
+//  --------------------
+//
+//    import {AuthGuard} from "@nestjs/passport";
+//    import {ExecutionContext, Injectable} from "@nestjs/common";
+//
+//    @Injectable()
+//    export class LocalAuthGuard extends AuthGuard('local') {
+//            async canActivate(context:ExecutionContext) {
+//                const result = (await super.canActivate(context)) as boolean;
+//                const request = context.switchToHttp().getRequest();
+//
+//                await super.logIn(request);
+//                return result
+//            }
+//    }
+//  --------------------
+
+
+
+// using JWT token authentication
+// ---------------------
+
+import {AuthGuard} from "@nestjs/passport";
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+
+}
+
